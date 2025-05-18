f85169dae2b19fc1b0bcb9184eb75107b210a04a
jackvandeleuv <106289788+jackvandeleuv@users.noreply.github.com>
2025-05-18 17:18:55 -0400

    commit

--- script.js
+++ script.js
@@ -1,25 +1,76 @@
 class Suggestion {
     constructor (article_id = -1, text = '') {
+        const cleanText = text.toUpperCase();
         this.article_id = article_id;
-        this.text = text;
+        this.text = cleanText;
+        this.clippedText = clipText(cleanText);
+        this.elem = this.makeElem();
+        addSuggestionListener(this.elem);
     }
 
-    getHTML() {
-        return `<div class="suggestionCard">${this.text.toUpperCase()}</div>`
+    makeElem() {
+        const topSuggestionCard = document.createElement('button');
+        topSuggestionCard.classList.add('suggestionCard');
+        topSuggestionCard.classList.add('noSelect')
+        topSuggestionCard.title = this.text;
+        topSuggestionCard.innerText = this.clippedText;
+        topSuggestionCard.id = `suggestion-${this.article_id}`;
+        return topSuggestionCard;
     }
 }
 
 class TopSuggestion {
     constructor (article_id = -1, text = '') {
+        const cleanText = text.toUpperCase();
         this.article_id = article_id;
-        this.text = text;
+        this.text = cleanText;
+        this.clippedText = clipText(cleanText);
+        this.elem = this.makeElem();
+        addSuggestionListener(this.elem);
+        this.addTopSuggestionListener();
     }
 
-    getHTML() {
-        return `<div id="topSuggestionCard" class="topSuggestionCard">${this.text.toUpperCase()}</div>`
+    addTopSuggestionListener() {
+        const form = document.getElementById('form-input');
+
+        form.addEventListener('keydown', function(event) {
+            // const topSuggestionCard = document.getElementById('topSuggestionCard');
+            if (event.key !== 'Enter') return;
+            // topSuggestionCard.style.backgroundColor = 'var(--dark-mid)';
+            // topSuggestionCard.style.color = 'var(--light-full)';
+
+        });
+
+        form.addEventListener('keyup', function(event) {
+            // const topSuggestionCard = document.getElementById('topSuggestionCard');
+            if (event.key !== 'Enter') return;
+            // topSuggestionCard.style.backgroundColor = 'var(--light-mid)';
+            // topSuggestionCard.style.color = 'var(--dark-full)';
+        });
+    }
+
+    makeElem() {
+        const topSuggestionCard = document.createElement('button');
+        topSuggestionCard.classList.add('topSuggestionCard');
+        topSuggestionCard.classList.add('noSelect')
+        topSuggestionCard.id = `suggestion-${this.article_id}`;
+        topSuggestionCard.title = this.text;
+        topSuggestionCard.innerText = this.clippedText;
+        return topSuggestionCard;
     }
 }
 
+function addSuggestionListener(element) {
+    element.addEventListener('click', async function(event) {
+        if (guessingArticle) return;
+
+        event.target.classList.add('suggestionHighlighted');
+
+        const article_id = event.target.id.split('-')[1];
+        await guessArticle(article_id, event.target.title);
+        event.target.classList.remove('suggestionHighlighted');
+    })
+}
 
 class TopGuessesBox {
     constructor (
@@ -34,20 +85,28 @@ class TopGuessesBox {
         this.article_id = article_id;
         this.title = title;
         this.chunk_id = chunk_id;
+        this.elem = this.getElem();
     }
 
-    getHTML() { 
+    getElem() { 
         const color = calculateDistanceColor(this.distance);
-        return `
-            <div title="${this.title.toUpperCase()}" class="topGuessesBox">
-                <div class="topGuessesTitle">
-                    <h4 style="background-color: ${color}; opacity">
-                        Distance: ${this.distance}
-                    </h4>
-                </div>
-                <p>${this.chunk}</p>
+        const topGuessesBox = document.createElement('div');
+        topGuessesBox.classList.add('topGuessesBox');
+        topGuessesBox.title = this.title.toUpperCase();
+
+        const innerHTML = `
+            <div class="topGuessesTitle" style="background-color: ${color};">
+                <h4>
+                    ${this.title}
+                </h4>
+                <h4>
+                    Distance: ${this.distance}
+                </h4>
             </div>
-        `
+        `;
+
+        topGuessesBox.innerHTML = innerHTML;
+        return topGuessesBox;
     }
 }
 
@@ -64,22 +123,38 @@ class GuessFeatureBox {
         this.article_id = article_id;
         this.rank = rank;
         this.chunk_id = chunk_id;
+        this.elem = this.makeElem();
     }
 
-    getHTML() {
+    makeElem() {
         const color = calculateDistanceColor(this.distance);
-        return `
-            <div class="guessFeatureBox">
-                <div class="guessFeatureTitle">
-                    <h3>(${this.rank})</h3>
-                    <div class="distanceBox" style="background-color: ${color};">
-                        <h3>Distance ${this.distance}</h3>
-                    </div>
+
+        const guessFeatureBox = document.createElement('div');
+        guessFeatureBox.classList.add('guessFeatureBox');
+
+        const innerHTML = `
+            <div class="guessFeatureTitle">
+                <h3>(${this.rank})</h3>
+                <div class="distanceBox" style="background-color: ${color};">
+                    <h3>Distance ${this.distance}</h3>
                 </div>
-                <p>${this.chunk}</p>
             </div>
-        `
+            <p>${this.chunk}</p>
+        `;
+
+        guessFeatureBox.innerHTML = innerHTML;
+
+        return guessFeatureBox;
     }
+}
+
+function clipText(text) {
+    const MAX_LEN = 30;
+    const diff = text.length - MAX_LEN;
+    if (diff > 0) {
+        return text.slice(0, MAX_LEN) + '...';
+    }
+    return text;
 }
 
 function calculateDistanceColor(distance) {
@@ -95,10 +170,10 @@ function calculateDistanceColor(distance) {
 }
 
 function updateContainer(items, container) {
-    container.innerHTML = items
-        .map((item) => item.getHTML())
-        .join('\n');
-    add_enter_listener();
+    container.innerHTML = '';
+    for (const item of items) {
+        container.appendChild(item.elem)
+    }
 }
 
 async function getSuggestion(input) {
@@ -149,12 +224,13 @@ function add_form_listeners() {
     form.addEventListener("submit", async (event) => {
         event.preventDefault();
         if (topSuggestion.article_id === -1) return;
+        if (guessingArticle) return;
 
         const formInput = document.getElementById("form-input");
         formInput.disabled = true;
         formInput.value = '';
 
-        await guess_article(topSuggestion.article_id, topSuggestion.text);
+        await guessArticle(topSuggestion.article_id, topSuggestion.text);
         topSuggestion = new TopSuggestion();
 
         formInput.disabled = false;
@@ -162,34 +238,83 @@ function add_form_listeners() {
     })
 }
 
-function add_enter_listener() {
-    const form = document.getElementById('form-input');
-    const topSuggestionCard = document.getElementById('topSuggestionCard');
-
-    form.addEventListener('keydown', function(event) {
-        if (event.key !== 'Enter') return;
-        topSuggestionCard.style.backgroundColor = 'var(--dark-mid)';
-        topSuggestionCard.style.color = 'var(--light-full)';
-
-    });
-
-    form.addEventListener('keyup', function(event) {
-        if (event.key !== 'Enter') return;
-        topSuggestionCard.style.backgroundColor = 'var(--light-mid)';
-        topSuggestionCard.style.color = 'var(--dark-full)';
-    });
+async function getTargetTokens() {
+    const response = await fetch('http://127.0.0.1:5000/keywords');
+    const tokens = new Map(Object.entries(await response.json()));
+    const total = tokens.values().reduce((a, b) => a + b);
+    const normalizedTokens = new Map();
+    for (const token of tokens.keys()) {
+        const rawCount = tokens.get(token);
+        normalizedTokens.set(token, rawCount / total);
+    }
+    return tokens;
 }
 
-async function guess_article(article_id, title) {
-    const url = `http://127.0.0.1:5000/guess_article/${article_id}`;
+async function getTopTokens() {
+    await targetTokens;
+    const topTokens = new Map();
+    for (const token of guessTokens.keys()) {
+        if (!targetTokens.get(token)) continue;
+        const guessCount = guessTokens.get(token) / totalGuessTokens;
+        const targetCount = targetTokens.get(token);
+        topTokens.set(token, guessCount * targetCount);
+    }
+}
+
+function urlToName(title) {
+    const titleSplit = title.split('/wiki/');
+    return titleSplit[titleSplit.length - 1];
+}
+
+async function getWikiImage(url) {
+    const name = urlToName(url);
+    const imageURL = `https://en.wikipedia.org/api/rest_v1/page/summary/${name}`;
+    console.log(imageURL)
+    const res = await fetch(imageURL, {headers:{'Accept': 'application/json'}});
+    if (!res.ok) return 'https://upload.wikimedia.org/wikipedia/commons/7/78/Claude_Monet_-_The_Magpie_-_Google_Art_Project.jpg';
+    const data = await res.json();
+    return data.originalimage?.source || data.thumbnail?.source || null;
+}
+
+function updateScoreBar() {
+    /**
+    *   bestScore is 2 (furthest) to 0 (closest).
+    *   2 - bestScore inverts the range.
+    *   Then we clip the score to go from 1 to 2.
+    */
+    const scoreBar = document.getElementById('scoreBar');
+    scoreBar.value = Math.max(2 - bestScore, 1) - 1;
+}
+
+async function guessArticle(article_id, title, limit=3) {
+    console.log(article_id)
+    guessingArticle = true;
+
+    const url = `http://127.0.0.1:5000/guess_article/${article_id}/limit/${limit}`;
     const guessFeatureBoxContainer = document.getElementById('guessFeatureBoxContainer');
     const topGuessesContainer = document.getElementById('topGuessesContainer');
     const topFeatureArticleTitle = document.getElementById('guessFeatureArticleTitle');
 
     guessFeatureBoxContainer.innerHTML = "<progress></progress";
-    topFeatureArticleTitle.innerHTML = `<h2>${title.toUpperCase()}</h2>`
+    topFeatureArticleTitle.innerHTML = `
+        <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; gap: 5px; width: 100%; padding: 10px;">
+            <img id="guessImage" class="guessImage" style="height: 200px;" src=""></img>
+            <h2>${title.toUpperCase()}</h2>
+        </div>
+    `;
 
     const chunks = await fetch(url).then(async (response) => await response.json());
+
+    const guessImage = document.getElementById('guessImage');
+    guessImage.src = await getWikiImage(chunks[0][3]);
+
+    const cleanChunks = chunks.map((chunk) => chunk[1].toLowerCase().trim());
+    for (const chunk of cleanChunks) {
+        if (!guessTokens.has(chunk)) continue;
+        const count = guessTokens.get(chunk);
+        guessTokens.set(chunk, count + 1);
+        totalGuessTokens += 1;
+    }
     
     const guessFeatureBoxes = chunks.map(
         (chunk, i) => new GuessFeatureBox(chunk[1], chunk[2], article_id, i + 1, chunk[0])
@@ -208,15 +333,34 @@ async function guess_article(article_id, title) {
     }
 
     topGuesses.sort((a, b) => a.distance - b.distance);
+
+    if (topGuesses) {
+        bestScore = Math.min(topGuesses[0].distance, bestScore);
+        updateScoreBar();
+    }
+
     updateContainer(topGuesses, topGuessesContainer);
 
-    const topSuggestionCard = document.getElementById('topSuggestionCard');
-    topSuggestionCard.style.backgroundColor = 'var(--light-mid)';
-    topSuggestionCard.style.color = 'var(--dark-full)';
+    const topSuggestionCard = document.getElementsByClassName('topSuggestionCard');
+    topSuggestionCard.backgroundColor = 'var(--light-mid)';
+    topSuggestionCard.color = 'var(--dark-full)';
+
+    console.log(`top tokens: ${await getTopTokens()}`);
+
+    guessingArticle = false;
 }
 
+// Globals.
 const topGuesses = [];
 const topGuessesIds = new Set();
 let topSuggestion = new TopSuggestion();
-add_enter_listener();
+
 add_form_listeners();
+
+const targetTokens = getTargetTokens();  // Normalized.
+const guessTokens = new Map();
+let totalGuessTokens = 0;
+
+let bestScore = 2;  // Range from 2 to 0.
+
+let guessingArticle = false;
--- server.py
+++ server.py
@@ -1,126 +1,18 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 import sqlite3
 from markupsafe import escape
 from flask_cors import CORS
 from copy import deepcopy
 import numpy as np
-from numpy.linalg import norm
-from numpy import dot
-import threading
-import time
 from dotenv import load_dotenv
 from openai import OpenAI
 import os 
+from collections import Counter
 
 load_dotenv()
 
 client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
 DB_PATH = 'preprocessing/data/data.db'
-
-def get_articles():
-    conn = sqlite3.Connection(DB_PATH)
-    cur = conn.cursor()
-    cur.execute("""
-        select 
-            article_id, clean_title, count, title 
-        from articles""")
-    articles = cur.fetchall()
-    articles = sorted(articles, key=lambda x: x[1])
-    conn.close()
-    return articles
-
-def get_daily_word_vector(article_id):
-    conn = sqlite3.Connection(DB_PATH)
-    cur = conn.cursor()
-    cur.execute("""
-        select vector
-        from embeddings
-        where article_id == ?
-    """, (article_id,))
-    vector = np.array([np.frombuffer(row[0]) for row in cur.fetchall()]).mean(axis=0)
-    conn.close()
-    return vector
-
-def get_daily_word_vector_live(article_id: int):
-    conn = sqlite3.Connection(DB_PATH)
-    cur = conn.cursor()
-    cur.execute("""
-        select chunk
-        from chunks
-        where article_id == ?
-    """, (article_id,))
-    chunks = [x[0] for x in cur.fetchall()]
-    result = client.embeddings.create(
-        input=chunks,
-        model="text-embedding-3-small"
-    )
-    return np.array([np.array(x.embedding) for x in result.data]).mean(axis=0)
-
-def bin_prefix_search(array, prefix, limit):
-    left = 0
-    right = len(array)
-    mid = ((right - left) // 2) + left
-    size = len(prefix)
-
-    found = False
-    while left < right:
-        if prefix == array[mid][1][: size]:
-            found = True
-            break
-        elif prefix < array[mid][1][: size]:
-            right = mid
-            mid = ((right - left) // 2) + left
-        else:
-            left = mid + 1
-            mid = ((right - left) // 2) + left
-
-    if not found:
-        return None
-            
-    left = mid
-    while 0 < left - 1 and prefix == array[left - 1][1][: size]:
-        left -= 1
-        
-    # Don't add one to comparisons with right if using slices because it is exclusive
-    right = mid
-    while right + 1 <= len(array) and prefix == array[right][1][: size]:
-        right += 1
-
-    array_slice = array[left : right]  # Shallow copy
-    for i in range(len(array_slice)):
-        if array_slice[i][1] == prefix:
-            copy = list(deepcopy(array_slice[i]))
-            copy[2] = 100000  # Higher than any count.
-            array_slice[i] = tuple(copy)  # Avoids mutating original list.
-    array_slice = sorted(array_slice, key=lambda x: x[2], reverse=True)[: limit]
-
-    return array_slice
-
-app = Flask(__name__)
-CORS(app)
-
-# Global variables.
-articles = get_articles()
-DAILY_WORD = 19017269
-# daily_vec = get_daily_word_vector(DAILY_WORD)
-daily_vec = get_daily_word_vector_live(DAILY_WORD)
-CHUNKS_TO_RETURN = 2
-
-@app.route("/suggestion/<q>/limit/<limit>")
-def suggestion(q, limit):
-    q = escape(q).lower()
-
-    try:
-        limit = int(escape(limit))
-    except ValueError as e:
-        print(e)
-        return "Invalid ID.", 404
-
-    result = bin_prefix_search(articles, q, limit)
-
-    if result:
-        return jsonify(result), 200
-    return "No matching article.", 404
 
 # def log_guess(guess_id, timestamp, ip):
 #     conn = sqlite3.Connection('stable/data/data.db')
@@ -178,49 +70,151 @@ def suggestion(q, limit):
 #     conn.close()
 
 #     return jsonify([])
-    
 
-@app.route("/guess_article/<article_id>")
-def guess_article(article_id):
+def get_articles():
+    conn = sqlite3.Connection(DB_PATH)
+    cur = conn.cursor()
+    cur.execute("""
+        select 
+            article_id, clean_title, count, title 
+        from articles""")
+    articles = cur.fetchall()
+    articles = sorted(articles, key=lambda x: x[1])
+    conn.close()
+    return articles
+
+def get_daily_word_vector_live(article_id: int):
+    conn = sqlite3.Connection(DB_PATH)
+    cur = conn.cursor()
+    cur.execute("""
+        select chunk
+        from chunks
+        where article_id == ?
+    """, (article_id,))
+    chunks = [x[0] for x in cur.fetchall()]
+    result = client.embeddings.create(
+        input=chunks,
+        model="text-embedding-3-small"
+    )
+    return np.array([np.array(x.embedding) for x in result.data]).mean(axis=0)
+
+def get_daily_word_tokens(article_id: int):
+    conn = sqlite3.Connection(DB_PATH)
+    cur = conn.cursor()
+    cur.execute("""
+        select chunk
+        from chunks
+        where article_id == ?
+    """, (article_id,))
+    chunks = ' '.join([x[0] for x in cur.fetchall()]).split()
+    chunks = [chunk.lower().strip() for chunk in chunks]
+    chunks = [(key, val) for key, val in Counter(chunks).items()]
+    chunks = list(sorted(chunks, key=lambda x: x[-1], reverse=True))[: 1000]
+    return chunks
+
+def bin_prefix_search(array, prefix, limit):
+    left = 0
+    right = len(array)
+    mid = ((right - left) // 2) + left
+    size = len(prefix)
+
+    found = False
+    while left < right:
+        if prefix == array[mid][1][: size]:
+            found = True
+            break
+        elif prefix < array[mid][1][: size]:
+            right = mid
+            mid = ((right - left) // 2) + left
+        else:
+            left = mid + 1
+            mid = ((right - left) // 2) + left
+
+    if not found:
+        return None
+            
+    left = mid
+    while 0 < left - 1 and prefix == array[left - 1][1][: size]:
+        left -= 1
+        
+    # Don't add one to comparisons with right if using slices because it is exclusive
+    right = mid
+    while right + 1 <= len(array) and prefix == array[right][1][: size]:
+        right += 1
+
+    array_slice = array[left : right]  # Shallow copy
+    for i in range(len(array_slice)):
+        if array_slice[i][1] == prefix:
+            copy = list(deepcopy(array_slice[i]))
+            copy[2] = 100000  # Higher than any count.
+            array_slice[i] = tuple(copy)  # Avoids mutating original list.
+    array_slice = sorted(array_slice, key=lambda x: x[2], reverse=True)[: limit]
+
+    return array_slice
+
+
+app = Flask(__name__)
+CORS(app)
+
+# Global variables.
+articles = get_articles()
+DAILY_WORD = 20353360
+daily_vec = get_daily_word_vector_live(DAILY_WORD)
+
+@app.route("/keywords")
+def keywords():
+    return get_daily_word_tokens(DAILY_WORD), 200
+
+@app.route("/suggestion/<q>/limit/<limit>")
+def suggestion(q, limit):
+    q = escape(q).lower()
+
+    try:
+        limit = int(escape(limit))
+    except ValueError as e:
+        print(e)
+        return "Invalid ID.", 404
+
+    result = bin_prefix_search(articles, q, limit)
+
+    if result:
+        return jsonify(result), 200
+    return "No matching article.", 404
+    
+@app.route("/guess_article/<article_id>/limit/<limit>")
+def guess_article(article_id, limit):
     try:
         conn = sqlite3.Connection('preprocessing/data/data.db')
         cur = conn.cursor()
 
         article_id = int(escape(article_id))
-        
+        limit = int(escape(limit))
+
         if article_id == DAILY_WORD:
-            print('YOU WON!')
             return jsonify([(-1, 'YOU WON!', 0)])
 
-        # cur.execute("""
-        #     select e.vector, e.chunk_id, c.chunk
-        #     from (
-        #         select vector, chunk_id 
-        #         from embeddings
-        #         where article_id == ?
-        #     ) as e
-        #     join (
-        #         select chunk_id, chunk
-        #         from chunks
-        #         where article_id == ?
-        #     ) as c
-        #         using (chunk_id)
-        # """, (article_id, article_id))
-
         cur.execute("""
-            select chunk_id, chunk
-            from chunks
-            where article_id == ?
-        """, (article_id,))
+            select chunk_id, chunk, url
+            from (
+                select chunk_id, chunk, article_id
+                from chunks
+                where article_id == ?
+            ) as c
+            join (
+                select article_id, url
+                from articles
+                where article_id == ?    
+            ) as a
+            on c.article_id == a.article_id
+        """, (article_id, article_id))
         guess = cur.fetchall()
+
         result = client.embeddings.create(
             input=[x[1] for x in guess],
             model="text-embedding-3-small"
         )
 
         guess_matrix = np.array([np.array(x.embedding) for x in result.data])
-
-        print('number of chunks:', guess_matrix.shape[0])
 
         # Vectorized cosine distance.
         # Faster than iteratively calling distance.cosine().
@@ -233,14 +227,9 @@ def guess_article(article_id):
         indices = np.argsort(distances)
 
         return [
-            (guess[i][0], guess[i][1], distances[i]) 
+            (guess[i][0], guess[i][1], distances[i], guess[i][2]) 
             for i in indices
-        ][: CHUNKS_TO_RETURN]
-
-        # return [
-        #     (guess[i][1], guess[i][2], distances[i]) 
-        #     for i in indices
-        # ][: CHUNKS_TO_RETURN]
+        ][: limit]
         
     except Exception as e:
         print(e)
