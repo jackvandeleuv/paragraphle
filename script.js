224300dc664f40b297a91826809960f8ee0af3d5
jackvandeleuv <106289788+jackvandeleuv@users.noreply.github.com>
2025-07-13 16:10:56 -0400

    Update backend path.

--- script.js
+++ script.js
@@ -559,7 +559,7 @@ async function guessArticle(article_id, title, articleURL, limit=5) {
 }
 
 // Globals.
-const API_URI = 'http://165.22.10.98/'
+const API_URI = 'http://api.hoiuyg324yjg4o26ku5h63.uk/'
 
 const topGuessesSorted = [];
 const topGuessesRef = [];
