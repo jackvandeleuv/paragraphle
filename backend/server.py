4f6b78aff686d905a8ea50bd7224516e8663471e
jackvandeleuv <106289788+jackvandeleuv@users.noreply.github.com>
2025-07-19 15:35:09 -0400

    update cors

--- backend/server.py
+++ backend/server.py
@@ -247,10 +247,10 @@ app = Flask(__name__)
 
 CORS(
     app,
-    resources={r"/.*": {
+    resources={r"/*": {
         "origins": [
             "https://jackvandeleuv.github.io",
-            "http://127.0.0.1:5500/"
+            "http://127.0.0.1:5500"
         ]
     }},
     methods=["GET", "OPTIONS"],
