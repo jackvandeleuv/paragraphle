6ede08a2578a1e842b5bfc7f3554eefee5d584e9
jackvandeleuv <106289788+jackvandeleuv@users.noreply.github.com>
2025-08-02 16:20:56 -0400

    Update image name

--- backend/go/server.go
+++ backend/go/server.go
@@ -19,18 +19,37 @@ func hi() string {
 }
 
 func main() {
+	db, err := sql.Open("sqlite", "../data.db")
+	if err != nil {
+		fmt.Println("Could not connect to database.")
+		return
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, world!")
+		fmt.Fprintln(w, "Hi!")
 	})
 
-	http.HandleFunc("/json", func(w http.ResponseWriter, r *http.Request) {
-		db, _ := sql.Open("sqlite", "test.db")
-		db.Exec("create table hi (integer primary key)")
-		// fmt.Println(err)
+	http.HandleFunc("/guess", func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("select article_id from articles limit 1")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer rows.Close()
 
-		q := r.URL.Query().Get("echo")
+		ids := make([]string, 0)
+		for rows.Next() {
+			var id string
+			if err := rows.Scan(&id); err != nil {
+				log.Fatal(err)
+			}
+			ids = append(ids, id)
+		}
+
+		fmt.Println(ids)
+
+		// q := r.URL.Query().Get("echo")
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(Message{Text: q})
+		json.NewEncoder(w).Encode(Message{Text: "test"})
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
